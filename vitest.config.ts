7248e491e795a4940333b2e7936f956a58e51caf
blck-snwmn <whitesnowmancreator@gmail.com>
2026-07-02 00:24:32 +0900

    fix test font loading

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
 		environment: 'jsdom',
 		globals: true,
 		css: true,
+		setupFiles: ['src/test-utils/setup.ts'],
 		coverage: {
 			provider: 'v8',
 			reporter: ['text', 'json', 'html'],
